11673147e81871a2b4229bc6dfc510d6a9b559b0
Luan Eike <luaneike.contato@gmail.com>
2023-08-22 21:37:41 -0300

    update option 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 string Mensagem = @"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -7,7 +8,11 @@ string Mensagem = @"
 ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
 ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
-List<string> listaDeBandas = new List<string>{"Neymar","Calypso","DJ Arana"};
+// List<string> listaDeBandas = new List<string>{"Neymar","Calypso","DJ Arana"};
+Dictionary<string, List<int>> listaDeBandas = new Dictionary<string, List<int>>();
+listaDeBandas.Add("Neymar",new List<int>{10, 8, 6});
+listaDeBandas.Add("Calypso",new List<int>{5, 0 ,2});
+listaDeBandas.Add("DJ Arana",new List<int>());
 
 void ExibirMensagem(){
     Console.WriteLine(Mensagem);
@@ -31,7 +36,7 @@ void ExibirMenu(){
             break;
         case 2: mostrarBandas();
             break;
-        case 3: Console.WriteLine("Você digitou a opção " + RespostaNum);
+        case 3: avaliarBandas();
             break;
         case 4: Console.WriteLine("Você digitou a opção " + RespostaNum);
             break;
@@ -45,7 +50,6 @@ void ExibirMenu(){
 }
 
 void tituloOpcao(string titulo){
-    Console.WriteLine();
     for(int i = 0; i < titulo.Length; i++){
         Console.Write("*");
     }
@@ -57,17 +61,20 @@ void tituloOpcao(string titulo){
 }
 
 // Opções
+
+//Opção 1
 void criarBanda(){
     Console.Clear();
     tituloOpcao("Registrar uma Banda");
     Console.Write("\nDigite o nome da banda: ");
     string nomeBanda = Console.ReadLine()!;
-    listaDeBandas.Add(nomeBanda);
+    listaDeBandas.Add(nomeBanda,new List<int>());
     Console.WriteLine($"A banda {nomeBanda} foi registrada com sucesso!");
     Thread.Sleep(1500);
     Console.Clear();
     ExibirMenu();
 }
+//Opção 2
 void mostrarBandas(){
     Console.Clear();
     tituloOpcao("Lista de Bandas");
@@ -75,7 +82,7 @@ void mostrarBandas(){
     //     Console.WriteLine($"- {listaDeBandas[i]}");
     // }
     Console.WriteLine();
-    foreach (string i in listaDeBandas){
+    foreach (string i in listaDeBandas.Keys){
         Console.WriteLine($"- {i}");        
     }
 
@@ -83,6 +90,25 @@ void mostrarBandas(){
     Console.ReadKey();
     Console.Clear();
     ExibirMenu();
+}
+//Opção 3
+void avaliarBandas(){
+    Console.Clear();
+    tituloOpcao("Avaliar Banda");
+    int num = 0;
+    foreach(string i in listaDeBandas.Keys){
+        num++;
+        Console.WriteLine($"{num} - {i}");
+    }
+    Console.Write("Digite a opção: ");
+    string escolhaBanda = Console.ReadLine()!;
+    int respostaBanda = int.Parse(escolhaBanda);
+    if (respostaBanda < listaDeBandas.Keys.Count+1 & respostaBanda > 0){
+        //voce precisa adicionar um switch que selecione a banda que deseja ser avaliada
+    }else{
+        Console.WriteLine("Wrong");
+    }
+
 }
 
 //
